8fb73595e078f9f7d93ccf16bce849a40eaeeb63
komitoff <tobo_91@abv.bg>
2017-05-18 20:12:47 +0300

    Ran the server

--- SimpleNodeServer/server/simpleServer.js
+++ SimpleNodeServer/server/simpleServer.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var port = 1234;
 
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-}).listen(9898);
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.write('Hello');
+    res.write('<html><ul><li>Simple Imtem</li></ul></html>')
+    res.end();
+}).listen(port);
 
-console.log('Server is Running ^_^');
+console.log('Server is Running ^_^ on port : ' + port);
