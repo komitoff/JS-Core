f09eb229b688fc12efae1337269e1153160f0135
komitoff <todorkomitov@gmail.com>
2017-11-15 20:22:32 +0200

    testing mongodb connection and simple queries (without mongoose)

--- node-example/index.js
+++ node-example/index.js
@@ -1,13 +1,31 @@
-console.log(15 + 3);
+const http = require('http')
 const fs = require('fs')
+const url = require('url')
 
-fs.readFile('index.js', 'utf8', (err, data) => {
-    if (err) {
-        console.log(err)
+const port = 7869
+
+http.createServer((req, res) => {
+    let path = url.parse(req.url).path
+
+    if (path === '/') {
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                console.log(err)
+                return
+            }
+
+            res.writeHead(200, {
+                'Content-Type' : 'text/html'
+            })
+            res.write(data)
+            res.end()
+        })
+    } else {
+        res.writeHead(404);
+        res.write('Page not found!')
+        res.end()
     }
-
-    console.log(data)
-    console.log('Finished')
 })
+.listen(port)
 
-
+console.log(`Server is running on port ${port} ...`)
