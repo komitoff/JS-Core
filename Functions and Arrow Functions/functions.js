4fd8160919f31bac555a836340add6221921d027
komitoff <todorkomitov@gmail.com>
2018-01-22 18:24:07 +0200

    Solved task 3

--- Functions and Arrow Functions/functions.js
+++ Functions and Arrow Functions/functions.js
@@ -48,3 +48,18 @@ function roadRadar(input) {
     }
 }
 
+function xmlFormatter(input) {
+    console.log(`<?xml version="1.0" encoding="UTF-8"?>\n<quiz>`)
+    for (let i = 0; i < input.length; i += 2) {
+        let question = input[i]
+        let answer = input[i + 1]
+        let tagged = addTags(question, answer)
+        console.log(tagged)
+    }
+
+    function addTags(question, answer) {
+        return `<question>\n${question}\n</question>\n<answer>\n${answer}\n</answer>`
+    }
+
+    console.log(`</quiz>`)
+}
