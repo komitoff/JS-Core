e4ac32f83a789f618009d4c4c3f7f20c5c84963d
komitoff <todorkomitov@gmail.com>
2018-01-22 18:33:23 +0200

    Solved task 4

--- Functions and Arrow Functions/functions.js
+++ Functions and Arrow Functions/functions.js
@@ -63,3 +63,56 @@ function xmlFormatter(input) {
 
     console.log(`</quiz>`)
 }
+
+function cookingByNumber(input) {
+    let number = Number(input[0])
+
+    for (let i = 1; i < input.length; i++) {
+        switch (input[i]) {
+            case 'dice':
+                number = dice(number)
+                console.log(number)
+                break
+            case 'chop':
+                number = chop(number)
+                console.log(number)
+                break
+            case 'spice':
+                number = spice(number)
+                console.log(number)
+                break
+            case 'bake':
+                number = bake(number)
+                console.log(number)
+                break
+            case 'fillet':
+                number = fillet(number)
+                console.log(number)
+                break
+            default:
+                break
+        }        
+    }
+
+    function chop(number) {
+        return number / 2
+    }
+
+    function dice(number) {
+        return Math.sqrt(number)
+    }
+
+    function spice(number) {
+        return number + 1
+    }
+
+    function bake(number) {
+        return number * 3
+    }
+
+    function fillet(number) {
+        return number - (number * 0.2)
+    }
+}
+
+cookingByNumber([32, 'chop', 'chop', 'chop', 'chop', 'chop'])
