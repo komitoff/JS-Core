45fe8fdd413f5f58feb062238d85bb8087551493
komitoff <todorkomitov@gmail.com>
2018-01-24 18:10:20 +0200

    Finished task 2

--- Arrays And Matrixes/arrays.js
+++ Arrays And Matrixes/arrays.js
@@ -1,3 +1,14 @@
+//#1 - prints array with give delimiter
 function delimiter(input) {
     return input.join(input.pop());
 }
+
+//#2 - print array value on given step
+function elementsOnGivenStep(array) {
+  let step = Number(array[array.length - 1])
+  for (let i = 0; i < array.length - 1; i += step) {
+    console.log(array[i])
+  }
+}
+
+elementsOnGivenStep([5, 20, 31, 4, 20, 2])
