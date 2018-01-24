88eb165d06df94c4bd9903324c9586dd363f635f
komitoff <todorkomitov@gmail.com>
2018-01-24 18:19:27 +0200

    Finished task 3*

--- Arrays And Matrixes/arrays.js
+++ Arrays And Matrixes/arrays.js
@@ -11,4 +11,25 @@ function elementsOnGivenStep(array) {
   }
 }
 
-elementsOnGivenStep([5, 20, 31, 4, 20, 2])
+//#3 - Add or remove element from any array
+function addAndRemove(array) {
+  let myArr = []
+  for (let i = 0; i < array.length; i++) {
+    if ('add' === array[i]) {
+      myArr.push(i + 1)
+    } else if ('remove' === array[i]) {
+      myArr.pop()
+    }
+  }
+
+  if (myArr.length === 0) {
+    console.log('Empty')
+    return
+  }
+
+  for (let j = 0; j < myArr.length; j++) {
+    console.log(myArr[j])
+  }
+}
+
+addAndRemove(['add', 'add', 'remove', 'add', 'add'])
