29cde10a72a0a01d89313c63fe65cd6a3a2387a8
komitoff <todorkomitov@gmail.com>
2018-02-26 18:35:12 +0200

    Did few more tasks

---
+++ JS - Advanced/Functions/bmi.js
@@ -0,0 +1,27 @@
+function calcBMI(name, age, weight, height) {
+
+  let personObj = {
+    name: name,
+    personalInfo: {
+      age: age,
+      weight: weight,
+      height: height,
+    }
+  }
+
+  height = height / 100;
+  personObj.BMI = Math.round(weight / (height * height));
+
+  if (personObj.BMI < 18.5) {
+    personObj.status = 'underweight'
+  } else if (personObj.BMI < 25) {
+    personObj.status = 'normal'
+  } else if (personObj.BMI < 30) {
+    personObj.status = 'overweight'
+  } else if (personObj.BMI >= 30) {
+    personObj.status = 'obese';
+    personObj.recommendation = 'admission required'
+ }
+
+  return (personObj);
+}
---
+++ JS - Advanced/Functions/functional-sum.js
@@ -0,0 +1,13 @@
+(function solve() {
+  let sum = 0;
+  
+  function add(value) {
+    sum += value;
+    return add;
+  }
+
+  add.toString= function () {
+    return sum;
+  }
+  return add;
+})();
