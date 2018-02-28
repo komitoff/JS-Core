a5a9c0dfed333e9cb721233aed080c7967f60052
komitoff <todorkomitov@gmail.com>
2018-02-28 19:49:58 +0200

    write unit tests for simple sum function

--- JS - Advanced/Unit tests/lab/test/example-test.js
+++ JS - Advanced/Unit tests/lab/test/example-test.js
@@ -2,7 +2,7 @@ const expect = require("chai").expect;
 function sum(arr) {
   let sum = 0
   for (num of arr) {
-    sum += num
+    sum += Number(num)
   }
   return sum
 }
@@ -10,6 +10,18 @@ function sum(arr) {
 
 describe('sum(arr)', function() {
   it('should return 3 for [1,2]', function() {
-    expect(sum([1,2])).to.be.equal(3);
+    expect(sum([1,2])).to.be.equal(3)
+  })
+  it('should return 1 for [1]', function() {
+    expect(sum([1])).to.be.equal(1)
+  })
+  it('should return 0 for []', function() {
+    expect(sum([])).to.be.equal(0)
+  })
+  it('should retur NaN for invalid input', function() {
+    expect(sum(['Invalid input!'])).to.be.NaN
+  })
+  it('should return 5 for [1.4, 1.6, 2]', function() {
+    expect(sum([1.4, 1.6, 2])).to.be.equal(5)
   })
 })
