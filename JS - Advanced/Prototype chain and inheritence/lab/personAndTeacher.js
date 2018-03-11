990b87b3acedb9642cf79c1aa32bd341b46e2164
komitoff <todorkomitov@gmail.com>
2018-03-11 17:11:33 +0200

    Extended class Person with Teacher and Student

--- JS - Advanced/Prototype chain and inheritence/lab/personAndTeacher.js
+++ JS - Advanced/Prototype chain and inheritence/lab/personAndTeacher.js
@@ -4,6 +4,11 @@ function solve() {
       this.name = name
       this.email = email
     }
+
+    toString() {
+      let className = this.constructor.name
+      return `${className} (name: ${this.name}, email: ${this.email})`
+    }
   }
 
   class Teacher extends Person {
@@ -11,7 +16,27 @@ function solve() {
       super(name, email)
       this.subject = subject
     }
+
+    toString() {
+      let parentString = super.toString().slice(0, -1)
+      return parentString + `, subject: ${this.subject})`
+    }
   }
 
-  return {Person, Teacher}
+  class Student extends Person {
+    constructor(name, email, course) {
+      super(name, email)
+      this.course = course
+    }
+
+    toString() {
+      let parentString = super.toString().slice(0, -1)
+      return parentString + `, course: ${this.course})`
+    }
+  }
+
+  return {Person, Teacher, Student}
 }
+
+'Student (name: Stamat, email: stamcho@stamishteto.bg, course: 666'
+'Student (name: Stamat, email: stamcho@stamishteto.bg, course: 666)'
