9e1efa91f088e78d3ad5d78926456c7b49e484f7
komitoff <todorkomitov@gmail.com>
2018-03-11 19:27:16 +0200

    Added function which extends class that comes as parameter.

---
+++ JS - Advanced/Prototype chain and inheritence/lab/extendFunction.js
@@ -0,0 +1,6 @@
+function extender(classToExtend) {
+  classToExtend.prototype.species = 'Human'
+  classToExtend.prototype.toSpeciesString = function () {
+    return `I am a ${this.species}. ${this.toString()}`
+  }
+}
