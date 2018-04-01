b6a930f7425654c7a3db8d93ea6e2907d3442426
komitoff <todorkomitov@gmail.com>
2018-04-01 17:06:38 +0300

    Final SPA project

--- JS - Applications/Book-Library/scripts/crudOperations.js
+++ JS - Applications/Book-Library/scripts/crudOperations.js
@@ -27,61 +27,92 @@ function registerUser() {
         headers: AUTH_HEADERS,
         data: { username, password }
     }).then(function (res) {
-        console.log('Here!');
         signInUser(res, 'Registration successful.')
     }).catch(handleAjaxError)
 }
 
 function listBooks() {
-    // TODO
-    // GET -> BASE_URL + 'appdata/' + APP_KEY + '/books'
-    // displayPaginationAndBooks(res.reverse())
+    $.ajax({
+        url: BASE_URL + 'appdata/' + APP_KEY + '/books',
+        headers: { 'Authorization': 'Kinvey ' + sessionStorage.getItem('authToken') }
+    }).then(function (res) {
+        showView('viewBooks')
+        displayPaginationAndBooks(res.reverse())
+    }).catch(handleAjaxError)
 }
 
 
 function createBook() {
-    // TODO
-    // POST -> BASE_URL + 'appdata/' + APP_KEY + '/books'
-    // showInfo('Book created.')
+    let author = $('#formCreateBook input[name=author]').val()
+    let title = $('#formCreateBook input[name=title]').val()
+    let description = $('#formCreateBook textarea[name=description]').val()
+    $.ajax({
+        method: 'POST',
+        url: BASE_URL + 'appdata/' + APP_KEY + '/books',
+        data: { author, title, description },
+        headers: { 'Authorization': 'Kinvey ' + sessionStorage.getItem('authToken') }
+    }).then(function (res) {
+        listBooks()
+        showInfo('Book created.')
+    }).catch(handleAjaxError)
 }
 
 function deleteBook(book) {
-    // TODO
-    // DELETE -> BASE_URL + 'appdata/' + APP_KEY + '/books/' + book._id
-    // showInfo('Book deleted.')
+    $.ajax({
+        method: 'DELETE',
+        url: BASE_URL + 'appdata/' + APP_KEY + '/books/' + book._id,
+        headers: { 'Authorization': 'Kinvey ' + sessionStorage.getItem('authToken') }
+    }).then(function () {
+        listBooks()
+        showInfo('Book deleted.')
+    }).catch(handleAjaxError)
 }
 
 function loadBookForEdit(book) {
-    // TODO
+    showView('viewEditBook')
+    $('#formEditBook input[name=id]').val(book._id)
+    $('#formEditBook input[name=title]').val(book.title)
+    $('#formEditBook input[name=author]').val(book.author)
+    $('#formEditBook textarea[name=description]').val(book.description)
 }
 
 function editBook() {
-    // TODO
-    // PUT -> BASE_URL + 'appdata/' + APP_KEY + '/books/' + book._id
-    // showInfo('Book edited.')
-}
+    let id = $('#formEditBook input[name=id]').val()
+    let title = $('#formEditBook input[name=title]').val()
+    let author = $('#formEditBook input[name=author]').val()
+    let description = $('#formEditBook textarea[name=description]').val()
 
-function saveAuthInSession(userInfo) {
-    // TODO
+    $.ajax({
+        method: 'PUT',
+        url: BASE_URL + 'appdata/' + APP_KEY + '/books/' + id,
+        headers: { 'Authorization': 'Kinvey ' + sessionStorage.getItem('authToken') },
+        data: { title, author, description }
+    }).then(function (res) {
+        listBooks()
+        showView('viewBooks')
+        showInfo('Book edited.')
+    })
 }
 
 function logoutUser() {
-    // TODO
-    // showInfo('Logout successful.')
+    sessionStorage.clear()
+    showHomeView()
+    showHideMenuLinks()
+    showInfo('Logout successful.')
 }
 
 function signInUser(res, message) {
-    sessionStorage.setItem('username', res.username);
-    sessionStorage.setItem('authtoken', res._kmd.authtoken);
-    sessionStorage.setItem('userId', res._id);
-    showHomeView();
-    showHideMenuLinks();
-    showInfo(message);
+    sessionStorage.setItem('username', res.username)
+    sessionStorage.setItem('authToken', res._kmd.authtoken)
+    sessionStorage.setItem('userId', res._id)
+    showHomeView()
+    showHideMenuLinks()
+    showInfo(message)
 }
 
 function displayPaginationAndBooks(books) {
     let pagination = $('#pagination-demo')
-    if(pagination.data("twbs-pagination")){
+    if (pagination.data("twbs-pagination")) {
         pagination.twbsPagination('destroy')
     }
     pagination.twbsPagination({
@@ -90,12 +121,35 @@ function displayPaginationAndBooks(books) {
         next: 'Next',
         prev: 'Prev',
         onPageClick: function (event, page) {
-            // TODO remove old page books
+            let table = $('#books > table')
+            table.find('tr').each((index, el) => {
+                if (index > 0) {
+                    $(el).remove()
+                }
+            })
             let startBook = (page - 1) * BOOKS_PER_PAGE
             let endBook = Math.min(startBook + BOOKS_PER_PAGE, books.length)
             $(`a:contains(${page})`).addClass('active')
             for (let i = startBook; i < endBook; i++) {
-                // TODO add new page books
+                let tr = $(`<tr>`)
+                table.append(
+                    $(tr).append($(`<td>${books[i].title}</td>`))
+                        .append($(`<td>${books[i].author}</td>`))
+                        .append($(`<td>${books[i].description}</td>`))
+                )
+                if (books[i]._acl.creator === sessionStorage.getItem('userId')) {
+                    $(tr).append(
+                        $(`<td>`).append(
+                            $(`<a href="#">[Edit]</a>`).on('click', function () {
+                                loadBookForEdit(books[i])
+                            })
+                        ).append(
+                            $(`<a href="#">[Delete]</a>`).on('click', function () {
+                                deleteBook(books[i])
+                            })
+                        )
+                    )
+                }
             }
         }
     })
