a1d08220db335404463b36e7f08236f0eb8c7041
komitoff <todorkomitov@gmail.com>
2018-04-24 16:41:57 +0300

    fixed paths problem

--- JS - Applications/Exam Prep/js/app.js
+++ JS - Applications/Exam Prep/js/app.js
@@ -1,3 +1,26 @@
 $(() => {
-  
-})
+  const app = Sammy('#container', function () {
+    this.use('Handlebars', 'hbs');
+
+    this.get('#/home', getWelcomePage);
+    this.get('exam.html', getWelcomePage);
+    this.get("", getWelcomePage);
+
+
+    //helper function
+    function getWelcomePage(ctx) {
+      if (!auth.isAuth()) {
+        ctx.loadPartials({
+          header: './templates/common/header.hbs',
+          loginForm: './templates/forms/login-form.hbs',
+          registerForm: './templates/forms/register-form.hbs',
+          footer: './templates/common/footer.hbs'
+        }).then(function () {
+          this.partial('./templates/welcome-anonymous.hbs');
+        });
+      }
+    }
+  });
+
+  app.run();
+});
