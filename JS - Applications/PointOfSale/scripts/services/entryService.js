03f6f5fba47446bc78e03b29732c44de39f4f82f
komitoff <todorkomitov@gmail.com>
2018-04-25 18:37:10 +0300

    implemented entryService (not tested)

---
+++ JS - Applications/PointOfSale/scripts/services/entryService.js
@@ -0,0 +1,22 @@
+let entryService = (() => {
+
+  function getEntriesByReceiptId(receiptId) {
+    const endpoint = `entries?query={"${receiptId}":"receiptId"}`;
+    remote.get('appdata', endpoint, 'kinvey');
+  }
+
+  function addEntry(type, qty, price, receiptId) {
+    const endpoint = `entries`;
+    let data = { type, qty, price, receiptId };
+    remote.post('appdata', endpoint, 'kinvey', data);
+  }
+
+  function deleteEntry(entryId) {
+    const endpoint = `entries/${entryId}`;
+    remote.remove('appdata', endpoint, 'kinvey');
+  }
+
+  return {
+
+  }
+})();
